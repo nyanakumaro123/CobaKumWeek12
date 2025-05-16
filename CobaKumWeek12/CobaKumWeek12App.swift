9e12b21bc01f9498468c82541944634fd17cc23e
JesslynLevana <jlevanahalim@student.ciputra.ac.id>
2025-05-16 12:18:26 +0700

    Initial Commit

---
+++ CobaKumWeek12/CobaKumWeek12App.swift
@@ -0,0 +1,17 @@
+//
+//  CobaKumWeek12App.swift
+//  CobaKumWeek12
+//
+//  Created by student on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CobaKumWeek12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CobaKumWeek12UITests/CobaKumWeek12UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CobaKumWeek12UITestsLaunchTests.swift
+//  CobaKumWeek12UITests
+//
+//  Created by student on 16/05/25.
+//
+
+import XCTest
+
+final class CobaKumWeek12UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
