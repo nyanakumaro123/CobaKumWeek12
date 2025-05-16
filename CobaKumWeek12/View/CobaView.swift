2392743e6ee840c374cb68219ec720e50ac6204a
nyanakumaro123 <nkumaroiek@student.ciputra.ac.id>
2025-05-16 14:17:40 +0700

    coba

---
+++ CobaKumWeek12/View/CobaView.swift
@@ -0,0 +1,18 @@
+//
+//  CobaView.swift
+//  CobaKumWeek12
+//
+//  Created by NKIS on 16/05/25.
+//
+
+import SwiftUI
+
+struct CobaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CobaView()
+}
